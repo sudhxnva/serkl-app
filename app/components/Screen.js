92a4a6c3c401dd2680b4c43899637f147725a991
Sudhanva M <sudhanva.m@pm.me>
2022-06-09 20:42:48 +0530

    Add screen and text components

---
+++ app/components/Screen.js
@@ -0,0 +1,11 @@
+import { SafeAreaView, StyleSheet } from "react-native";
+import React from "react";
+import Constants from "expo-constants";
+
+export default function Screen({ children, style }) {
+  return <SafeAreaView style={[styles.screen, style]}>{children}</SafeAreaView>;
+}
+
+const styles = StyleSheet.create({
+  screen: { marginTop: Constants.statusBarHeight, flex: 1 },
+});
---
+++ app/components/Text.js
@@ -0,0 +1,12 @@
+import { Text } from "react-native";
+import React from "react";
+
+import defaultStyles from "../config/styles";
+
+export default function AppText({ children, style, ...otherProps }) {
+  return (
+    <Text style={[defaultStyles.text, style]} {...otherProps}>
+      {children}
+    </Text>
+  );
+}
